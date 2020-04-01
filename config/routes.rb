fa88e431cf3e0955f50e2a50015178cf2829f267
tosiran <f2.ree_eat_10470@icloud.com>
2020-04-01 17:45:15 +0900

    新規登録ページ　作成中

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,26 @@
 Rails.application.routes.draw do
-  get 'card/new'
 
-  get 'card/show'
 
   root to: 'users#index'
   get 'users/index'
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
-  
+
   resources :card, only: [:new, :show] do
     collection do
       post 'show', to: 'card#show'
       post 'pay', to: 'card#pay'
       post 'delete', to: 'card#delete'
+    end
+  end
+
+
+resources :purchase, only: [:index] do
+    collection do
+      get 'index', to: 'purchase#index'
+      post 'pay', to: 'purchase#pay'
+      get 'done', to: 'purchase#done'
     end
   end
 end
